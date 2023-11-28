c48fd49efba5ef39db8d21d546399ff420f294bb
Eswar S Sethu <63389431+Eswar-S-Sethu@users.noreply.github.com>
2023-11-28 14:15:06 +1100

    changes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,47 @@
-const clickMe=()=>{
-    alert("Thanks for clicking me. Hope you have a nice day");
-}
+const cardList = [
+    {
+        title: "Kitten 2",
+        image: "https://image.petmd.com/files/styles/863x625/public/2023-04/kitten-development.jpeg",
+        link: "About Kitten 2",
+        description: "Demo description about kitten 2"
+    },
+    {
+        title: "Kitten 3",
+        image: "https://www.zooplus.co.uk/magazine/wp-content/uploads/2021/01/striped-grey-kitten.jpg",
+        link: "About Kitten 3",
+        description: "Demo description about kitten 3"
+    }
+];
 
-$(document).ready(function(){
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+    console.log("Form Data Submitted: ", formData);
+}
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.desciption + '</p>' +
+            '</div></div></div>';
+        $("#card-section").append(itemToAppend)
+    });
+}
+$(document).ready(function () {
     $('.materialboxed').materialbox();
-    $('#clickMeButton').click(()=>{
-        clickMe();
+    $('#formSubmit').click(() => {
+        submitForm();
     })
+    addCards(cardList);
+    $('.modal').modal();
 });
